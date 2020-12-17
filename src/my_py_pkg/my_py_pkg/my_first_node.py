ec330e551d759616c7d369512241ff493bb17510
Adam LeCorney <adamlecorney@gmail.com>
2020-12-17 01:50:48 +0100

    extend simple python node with timer callback

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -3,10 +3,23 @@ import rclpy
 from rclpy.node import Node
 
 
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__('py_test')
+        self.counter = 0
+        self.get_logger().info('Initialized a MyNode')
+        # inherited from Node base class
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter += 1
+        self.get_logger().info('Hello from callback, counter: ' + str(self.counter))
+
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node('py_test')
-    node.get_logger().info('Hello ROS2')
+    node = MyNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
