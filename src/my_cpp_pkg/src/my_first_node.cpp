e28c26585fef2f6912b121cecd38a13912c67426
Adam LeCorney <adamlecorney@gmail.com>
2020-12-18 00:25:56 +0000

    extend simple cpp node with timer callback

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,10 +1,29 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node
+{
+public:
+  MyNode() : Node("cpp_test"), counter_(0)
+  {
+    RCLCPP_INFO(this->get_logger(), "Initialized Cpp Node");
+    timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&MyNode::timerCallback, this));
+  }
+
+private:
+  void timerCallback()
+  {
+    counter_++;
+    RCLCPP_INFO(this->get_logger(), "Hello from timerCallback, %d", counter_);
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  int counter_;
+};
+
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<rclcpp::Node>("cpp_test");
-  RCLCPP_INFO(node->get_logger(), "Hello Cpp Node");
+  auto node = std::make_shared<MyNode>();
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
