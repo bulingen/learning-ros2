553ad80901d46778f80be7d9682c8df41b748ce6
bulingen <adamlecorney@gmail.com>
2020-12-22 01:27:41 +0100

    add simple subscriber

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -21,7 +21,8 @@ setup(
     entry_points={
         'console_scripts': [
             'py_node = my_py_pkg.my_first_node:main',
-            "robot_news_station = my_py_pkg.robot_news_station:main"
+            "robot_news_station = my_py_pkg.robot_news_station:main",
+            "smartphone = my_py_pkg.smartphone:main"
         ],
     },
 )
