b5aaff6f82bea66e1b4c8d8109135a3e0c640908
bulingen <adamlecorney@gmail.com>
2020-12-26 18:30:35 +0100

    add simple python client

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -23,7 +23,8 @@ setup(
             'py_node = my_py_pkg.my_first_node:main',
             "robot_news_station = my_py_pkg.robot_news_station:main",
             "smartphone = my_py_pkg.smartphone:main",
-            "add_two_ints_server = my_py_pkg.add_two_ints_server:main"
+            "add_two_ints_server = my_py_pkg.add_two_ints_server:main",
+            "add_two_ints_client_no_oop = my_py_pkg.add_two_ints_client_no_oop:main"
         ],
     },
 )
