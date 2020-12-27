76a9848a06fd666f95de6502765dad742e7ba122
bulingen <adamlecorney@gmail.com>
2020-12-27 01:09:03 +0100

    expand number counter with a service for resetting the counter

--- src/cpp_activity2/src/number_counter.cpp
+++ src/cpp_activity2/src/number_counter.cpp
@@ -1,5 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
 
 class NumberCounterNode : public rclcpp::Node
 {
@@ -11,6 +16,10 @@ public:
           "number",
           10,
           std::bind(&NumberCounterNode::handleNumberMessage, this, std::placeholders::_1) 
+      );
+      server_ = this->create_service<example_interfaces::srv::SetBool>(
+          "reset_counter",
+          std::bind(&NumberCounterNode::handleResetCounterRequest, this, _1, _2)
       );
       RCLCPP_INFO(this->get_logger(), "Number counter has been initialized");
   }
@@ -25,8 +34,31 @@ private:
         newMessage.data = counter_;
         publisher_->publish(newMessage);
     }
+
+    void handleResetCounterRequest(
+        const example_interfaces::srv::SetBool::Request::SharedPtr request,
+        const example_interfaces::srv::SetBool::Response::SharedPtr response
+    )
+    {
+        bool should_reset = request->data;
+        if (should_reset)
+        {
+          counter_ = 0;
+          RCLCPP_INFO(this->get_logger(), "Counter was reset");
+          response->success = true;
+          response->message = "Counter was reset";
+        }
+        else 
+        {
+          RCLCPP_INFO(this->get_logger(), "Pass true to reset counter");
+          response->success = false;
+          response->message = "Pass true to reset counter";
+        }
+    }
+
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr subscriber_;
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
+    rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr server_;
     int counter_;
 };
 
