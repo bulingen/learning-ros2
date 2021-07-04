71ea173119b8acfec843f30d44acf24d37c343c0
bulingen <adamlecorney@gmail.com>
2021-07-05 00:22:52 +0200

    add arguments to the nodes in the launch file

--- src/my_robot_bringup/launch/number_app.launch.py
+++ src/my_robot_bringup/launch/number_app.launch.py
@@ -5,14 +5,29 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
+    remap_number_topic = ("number", "my_number")
+
     number_publisher_node = Node(
         package="activity2",
-        executable="number_publisher"
+        executable="number_publisher",
+        name="my_number_publisher",
+        remappings=[
+            remap_number_topic
+        ],
+        parameters=[
+            {"number_to_publish": 4},
+            {"publish_frequency": 0.5},
+        ]
     )
 
     number_counter_node = Node(
         package="cpp_activity2",
-        executable="number_counter"
+        executable="number_counter",
+        name="my_number_counter",
+        remappings=[
+            remap_number_topic,
+            ("number_count", "my_number_count")
+        ]
     )
 
     ld.add_action(number_publisher_node)
