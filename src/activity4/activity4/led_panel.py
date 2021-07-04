1ab17200dc45fb3f10aa4d9ad2174c0e551f28f9
bulingen <adamlecorney@gmail.com>
2021-07-04 19:09:14 +0200

    make led_panel able to receive and handle requests

--- src/activity4/activity4/led_panel.py
+++ src/activity4/activity4/led_panel.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from activity4_interfaces.msg import LedState
+from activity4_interfaces.srv import SetLed
 
 
 class LedPanel(Node):
@@ -14,6 +15,7 @@ class LedPanel(Node):
 
         self.led_states_publisher_ = self.create_publisher(LedState, "led_states", 10)
         self.led_states_timer_ = self.create_timer(4, self.publish_led_states)
+        self.set_led_service_ = self.create_service(SetLed, "set_led", self.on_set_led)
         self.get_logger().info("LED panel node has been started")
 
     def publish_led_states(self):
@@ -23,6 +25,27 @@ class LedPanel(Node):
         msg.led_3_active = self.led_3_active
         msg.led_states = self.led_states
         self.led_states_publisher_.publish(msg)
+
+    def on_set_led(self, request, response):
+        led_number = request.led_number
+        state = request.state
+        
+        if led_number > len(self.led_states) or led_number <= 0:
+            response.success = False
+            return response
+
+        self.led_states[led_number - 1] = state
+
+        if led_number == 1:
+            self.led_1_active = state
+        elif led_number == 2:
+            self.led_2_active = state
+        elif led_number == 3:
+            self.led_3_active = state
+
+        response.success = True
+        self.publish_led_states()
+        return response
 
 
 def main(args=None):
