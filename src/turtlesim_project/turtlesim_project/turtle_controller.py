7a9a91cd418bbaaee3b458beccaccb85edad7555
bulingen <adamlecorney@gmail.com>
2021-07-12 00:12:34 +0200

    make turtle go to a hardcoded target

--- src/turtlesim_project/turtlesim_project/turtle_controller.py
+++ src/turtlesim_project/turtlesim_project/turtle_controller.py
@@ -9,22 +9,28 @@ COMMAND_TOPIC = '/turtle1/cmd_vel'  # Twist
 STATUS_TOPIC = '/turtle1/pose'  # Pose
 
 # top left (ish)
-GOAL = {'x': 1.0, 'y': 10.0}
+GOAL_TOP_LEFT = {'x': 1.0, 'y': 10.0}
 
+# bottom right (ish)
+GOAL_BOTTOM_RIGHT = {'x': 10.0, 'y': 1.0}
+GOAL_TOP_RIGHT = {'x': 10.0, 'y': 10.0}
+GOAL_BOTTOM_LEFT = {'x': 1.0, 'y': 1.0}
+
+GOAL = GOAL_BOTTOM_RIGHT
+
+# maybe send commands at some interval and calculate stuff at another?
 # 1 sec / 2 = 0.5
-COMMAND_PERIOD = 0.5
+COMMAND_PERIOD = 0.2
+
 
 # angle_vel * 2
 ANGLE_VELOCITY_COEFFICIENT = 2
 
-ANGLE_TOLERANCE = 0.2
+# a full lap is 2 * PI
+ANGLE_TOLERANCE = 0.1
 
-# Pose example
-# x: 5.3709917068481445
-# y: 5.558200359344482
-# theta: -0.1742475926876068
-# linear_velocity: 0.0
-# angular_velocity: 0.0
+# the canvas side is 11.0
+DISTANCE_TOLERANCE = 0.2
 
 
 def calculate_distance(pos1_x, pos1_y, pos2_x, pos2_y):
@@ -65,34 +71,33 @@ class TurtleController(Node):
         return secs + nsecs / 1000000000.0
 
     def handle_status_update(self, msg):
-        angle_to_target = calculate_angle(msg.x, msg.y, GOAL['x'], GOAL['y'])
-        distance_to_target = calculate_distance(GOAL['x'], GOAL['y'], msg.x, msg.y)
         time_now = self.get_current_time_in_seconds()
         if time_now - self.last_time_command_sent > COMMAND_PERIOD:
-            turn_angle = get_turn_angle(angle_to_target, msg.theta)
-            angle_velocity = ANGLE_VELOCITY_COEFFICIENT * turn_angle
+            distance_to_target = calculate_distance(GOAL['x'], GOAL['y'], msg.x, msg.y)
 
-            if abs(turn_angle) <= ANGLE_TOLERANCE:
-                print('we are on track')
-                angle_velocity = 0.0
+            linear_velocity = 0.0
+            angle_velocity = 0.0
 
-            else:
-                if turn_angle > 0:
-                    # we're to the right of target angle, so need to turn left
-                    print('turn left', turn_angle, 'velocity', angle_velocity)
-                    # angle_velocity = 0.4
-                    # angle_velocity = -angle_error
-                else:
-                    print('turn right', turn_angle, 'velocity', angle_velocity)
-                    # angle_velocity = -0.4
-                    # angle_velocity = -angle_error
+            has_somewhere_to_go = distance_to_target > DISTANCE_TOLERANCE
 
-            self.publish_command(angle_velocity)
+            if has_somewhere_to_go:
+                linear_velocity = 2.0
+
+                angle_to_target = calculate_angle(msg.x, msg.y, GOAL['x'], GOAL['y'])
+
+                turn_angle = get_turn_angle(angle_to_target, msg.theta)
+                angle_velocity = ANGLE_VELOCITY_COEFFICIENT * turn_angle
+
+                if abs(turn_angle) <= ANGLE_TOLERANCE:
+                    angle_velocity = 0.0
+
+            self.publish_command(linear_velocity, angle_velocity)
             self.last_time_command_sent = time_now
 
-    def publish_command(self, angle_velocity):
+    def publish_command(self, linear_velocity, angle_velocity):
         msg = Twist()
         linear = Vector3()
+        linear.x = linear_velocity
         angular = Vector3()
         angular.z = angle_velocity
         msg.linear = linear
