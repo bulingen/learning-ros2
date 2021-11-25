b12709893449499c84f4f225ce7ac2c61747e3ee
bulingen <adamlecorney@gmail.com>
2021-11-25 22:13:16 +0100

    simplify usage of python deps

--- source_me.sh
+++ source_me.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# this file should always be sourced when running ros2
 source $(echo $PWD)/install/setup.bash || echo -e "\nHave you run 'colcon build'?"
+
+# source python virtual env to isolate dependencies
 source ./venv/bin/activate
+
+# add site-packages from python venv to PYTHONPATH so that ros sees them
+PYTHON_VERSION=$(python -c"import sys; print(str(sys.version_info.major) + '.' + str(sys.version_info.minor))")
+LOCAL_PYTHON_PACKAGES="./venv/lib/python$PYTHON_VERSION/site-packages"
+export PYTHONPATH=$LOCAL_PYTHON_PACKAGES:$PYTHONPATH
